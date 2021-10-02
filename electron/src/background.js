93085cc1fd7be8edc183d89d2da0ba39dff7dd27
Timo Schwarzer <timo@schwarzer.dev>
2021-10-02 18:43:19 +0200

    Only create window if it's the primary app instance
    Closes #32

--- electron/src/background.js
+++ electron/src/background.js
@@ -25,130 +25,130 @@ if (isDevelopment) {
 
 if (!app.requestSingleInstanceLock()) {
   app.quit()
-}
+} else {
+  // Scheme must be registered before the app is ready
+  protocol.registerSchemesAsPrivileged([
+    { scheme: 'app', privileges: { secure: true, standard: true } },
+  ])
 
-// Scheme must be registered before the app is ready
-protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } },
-])
+  app.setAsDefaultProtocolClient('ori-rando')
 
-app.setAsDefaultProtocolClient('ori-rando')
+  async function createWindow() {
+    registerIpcApi()
 
-async function createWindow() {
-  registerIpcApi()
+    if (await SettingsService.importSettingsFromOldInstallation()) {
+      console.log('Successfully imported old settings.')
+    }
 
-  if (await SettingsService.importSettingsFromOldInstallation()) {
-    console.log('Successfully imported old settings.')
-  }
+    // Create the browser window.
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      autoHideMenuBar: true,
+      backgroundColor: '#050e17',
+      webPreferences: {
+        // Use pluginOptions.nodeIntegration, leave this alone
+        // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+        nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+        contextIsolation: true,
+        preload: path.join(__dirname, 'preload.js'),
+      },
+      frame: true,
+      show: false,
+      title: 'Ori and the Will of the Wisps Randomizer',
+    })
 
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    autoHideMenuBar: true,
-    backgroundColor: '#050e17',
-    webPreferences: {
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js'),
-    },
-    frame: true,
-    show: false,
-    title: 'Ori and the Will of the Wisps Randomizer',
-  })
+    win.maximize()
+    win.show()
 
-  win.maximize()
-  win.show()
-
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    // await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    await win.loadURL('http://localhost:3000/electron')
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
-  } else {
-    createProtocol('app')
-    // Load the index.html when not in development
-    await win.loadURL('app://./index.html#/electron')
-  }
-
-  const commandLineArgumentHandler = (args) => {
-    const lastArg = args[args.length - 1]
-
-    if (lastArg.endsWith('.wotwr') && fs.existsSync(lastArg)) {
-      // We got a seed!
-      win.focus()
-      win.webContents.send('main.openSeed', lastArg)
+    if (process.env.WEBPACK_DEV_SERVER_URL) {
+      // Load the url of the dev server if in development mode
+      // await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+      await win.loadURL('http://localhost:3000/electron')
+      if (!process.env.IS_TEST) win.webContents.openDevTools()
     } else {
-      // Maybe an URL?
-      try {
-        const url = new URL(lastArg)
-        if (url.protocol === 'ori-rando:') {
-          win.focus()
-          win.webContents.send('main.openUrl', url.toString())
+      createProtocol('app')
+      // Load the index.html when not in development
+      await win.loadURL('app://./index.html#/electron')
+    }
+
+    const commandLineArgumentHandler = (args) => {
+      const lastArg = args[args.length - 1]
+
+      if (lastArg.endsWith('.wotwr') && fs.existsSync(lastArg)) {
+        // We got a seed!
+        win.focus()
+        win.webContents.send('main.openSeed', lastArg)
+      } else {
+        // Maybe an URL?
+        try {
+          const url = new URL(lastArg)
+          if (url.protocol === 'ori-rando:') {
+            win.focus()
+            win.webContents.send('main.openUrl', url.toString())
+          }
+        } catch (e) {
+          console.warn('Could not parse URL', e)
         }
-      } catch (e) {
-        console.warn('Could not parse URL', e)
       }
     }
-  }
 
-  app.on('second-instance', (event, args) => commandLineArgumentHandler(args))
-  commandLineArgumentHandler(process.argv)
+    app.on('second-instance', (event, args) => commandLineArgumentHandler(args))
+    commandLineArgumentHandler(process.argv)
 
-  CrashDetectService.setOnCrashCallback(crashZipName => {
-    win.webContents.send('main.crashDetected', crashZipName)
-  })
-  await CrashDetectService.start()
-}
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS3_DEVTOOLS)
-    } catch (e) {
-      console.log('Vue Devtools failed to install:', e.toString())
-    }
-  }
-
-  try {
-    await createWindow()
-  } catch (e) {
-    console.log(e)
-  }
-})
-
-// Exit cleanly on request from parent process in development mode.
-if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit()
-      }
+    CrashDetectService.setOnCrashCallback(crashZipName => {
+      win.webContents.send('main.crashDetected', crashZipName)
     })
-  } else {
-    process.on('SIGTERM', () => {
+    await CrashDetectService.start()
+  }
+
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
       app.quit()
-    })
+    }
+  })
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', async () => {
+    if (isDevelopment && !process.env.IS_TEST) {
+      // Install Vue Devtools
+      try {
+        await installExtension(VUEJS3_DEVTOOLS)
+      } catch (e) {
+        console.log('Vue Devtools failed to install:', e.toString())
+      }
+    }
+
+    try {
+      await createWindow()
+    } catch (e) {
+      console.log(e)
+    }
+  })
+
+  // Exit cleanly on request from parent process in development mode.
+  if (isDevelopment) {
+    if (process.platform === 'win32') {
+      process.on('message', (data) => {
+        if (data === 'graceful-exit') {
+          app.quit()
+        }
+      })
+    } else {
+      process.on('SIGTERM', () => {
+        app.quit()
+      })
+    }
   }
 }

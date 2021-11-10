f05ddbdfb25744b1d8c50dff965963bc641f5620
Timo Schwarzer <timo@schwarzer.dev>
2021-11-10 21:24:57 +0100

    Allow setting relic probability

--- components/WotwSeedgenGoalSelect.vue
+++ components/WotwSeedgenGoalSelect.vue
@@ -1,27 +1,40 @@
 <template>
   <div>
-    <v-tooltip
-      v-for='(goal, id) in goals'
-      :key='id'
-      top
-      :disabled='!goal.description'
-      open-delay='500'
-    >
-      <template #activator='{on}'>
-        <v-btn
-          :value='true'
-          class='text-none mr-1 mb-1'
-          :color='goalStates[id] ? "secondary" : "background lighten-2"'
-          depressed
-          v-bind='$attrs'
-          v-on='on'
-          @click='goalStates[id] = !goalStates[id]'
-        >
-          {{ goal.name }}
-        </v-btn>
-      </template>
-      <span>{{ goal.description }}</span>
-    </v-tooltip>
+    <div>
+      <v-tooltip
+        v-for='(goal, id) in goals'
+        :key='id'
+        top
+        :disabled='!goal.description'
+        open-delay='500'
+      >
+        <template #activator='{on}'>
+          <v-btn
+            :value='true'
+            class='text-none mr-1 mb-1'
+            :color='goalStates[id] ? "secondary" : "background lighten-2"'
+            depressed
+            v-bind='$attrs'
+            v-on='on'
+            @click='goalStates[id] = !goalStates[id]'
+          >
+            {{ goal.name }}
+          </v-btn>
+        </template>
+        <span>{{ goal.description }}</span>
+      </v-tooltip>
+    </div>
+    <v-expand-transition>
+      <div v-if='!!goalStates["RELICS"]'>
+        <div class='mt-4'>
+          <v-slider
+            v-model="relicChance"
+            hide-details
+            :label='`${relicChance}% relic chance (per area)`'
+          />
+        </div>
+      </div>
+    </v-expand-transition>
   </div>
 </template>
 
@@ -41,12 +54,22 @@
     data: vm => ({
       goalStates: {},
       inputValue: vm.value ?? [],
+      relicChance: 60,
     }),
     watch: {
       goalStates: {
         deep: true,
         handler(value) {
-          this.inputValue = Object.keys(value).filter(p => !!this.goalStates[p])
+          this.inputValue = Object.keys(value)
+            .filter(p => !!this.goalStates[p])
+            .map(goal => {
+              switch (goal) {
+                case 'RELICS':
+                  return 'RELICS:' + this.relicChance + '%'
+              }
+
+              return goal
+            })
         },
       },
       inputValue: {
@@ -69,7 +92,7 @@
     methods: {
       updateStates() {
         for (const id of Object.keys(this.goals)) {
-          this.$set(this.goalStates, id, this.inputValue.includes(id))
+          this.$set(this.goalStates, id, this.inputValue.map(g => g.split(':')[0]).includes(id))
         }
       },
     },

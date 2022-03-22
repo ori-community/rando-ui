6f973f3ccc57e2335cc7d4133e4e760abb502918
Timo Schwarzer <timo@schwarzer.dev>
2022-03-22 22:30:32 +0100

    Simplify unique name algorithm

--- assets/lib/customHeader.js
+++ assets/lib/customHeader.js
@@ -4,34 +4,34 @@ import { EventBus } from '~/assets/lib/EventBus'
 export async function downloadHeaderToCustom(axios, headerName, displayName){
   try {
     const content = await fetchServerHeaderContent(axios, headerName)
-    insertCustomHeader(displayName, content)
+    await insertCustomHeader(displayName, content)
   } catch (e) {
     console.error(e)
     EventBus.$emit('notification', {
       message: 'An error occured while trying to download the header',
       color: 'error'
-    }) 
+    })
   }
 }
+
 async function fetchServerHeaderContent(axios, headerName) {
   return await axios.$get(`/seedgen/headers/${headerName}/file`)
 }
+
 export async function insertCustomHeader(headerName, headerContent){
   const headers = await (await getDb).customHeaders.toArray()
 
-  // get free name so the inserted header is easier to find
-  let freeName = ''
-  let count = -1
-  do {
-    count++
-    const comparisonName = headerName + `${count > 0 ? ` (${count.toString()})` : '' }`
-    if (!headers.find(element => element.name === comparisonName)){
-      freeName = comparisonName
-    }
-  } while (!freeName)
+  // get unique name so the inserted header is easier to find
+  let targetName = headerName
+  let nextSuffixNumber = 1
+  while (headers.some(h => h.name === targetName)) {
+    targetName = `${headerName} (${nextSuffixNumber})`
+    nextSuffixNumber++
+  }
 
-  saveCustomHeader(null, freeName, headerContent)
+  await saveCustomHeader(null, targetName, headerContent)
 }
+
 export async function saveCustomHeader(headerId, headerName, headerContent){
   const headerPayload = {
     name: headerName,

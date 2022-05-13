f314ce92acd98460f67a0ba9134a10f089c46259
Timo Schwarzer <timo@schwarzer.dev>
2022-05-13 19:18:18 +0200

    Show secondary windows only when they are ready

--- electron/src/lib/WindowService.ts
+++ electron/src/lib/WindowService.ts
@@ -4,14 +4,24 @@ import BrowserWindowConstructorOptions = Electron.BrowserWindowConstructorOption
 
 export class WindowService {
   static createWindow(options: BrowserWindowConstructorOptions) {
-    return new BrowserWindow({
+    const window = new BrowserWindow({
       autoHideMenuBar: true,
       webPreferences: {
         nodeIntegration: Boolean(process.env.ELECTRON_NODE_INTEGRATION),
         contextIsolation: true,
         preload: path.join(__dirname, 'preload.js'),
       },
+      paintWhenInitiallyHidden: true,
+      show: false,
+      backgroundColor: '#050e17',
+      darkTheme: true,
       ...options,
     })
+
+    window.on('ready-to-show', () => {
+      window.show()
+    })
+
+    return window
   }
 }

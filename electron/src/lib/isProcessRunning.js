3452cef2e9296424208667134f1d25def2b4f5f1
Timo Schwarzer <timo@schwarzer.dev>
2022-11-20 23:13:48 +0100

    Fix launching the randomizer

--- electron/src/lib/isProcessRunning.js
+++ electron/src/lib/isProcessRunning.js
@@ -1,6 +1,34 @@
-import psList from 'ps-list'
+import { exec } from 'child_process'
+import { parse as parseCsv } from 'csv-parse'
+import os from 'os'
 
 export const isProcessRunning = async (processName) => {
-  const processes = await psList({ all: true })
-  return processes.some(p => p.name.toLowerCase() === processName.toLowerCase())
+  if (os.platform() !== 'win32') {
+    console.log('isProcessRunning used on unsupported OS')
+    return false
+  }
+
+  const tasklistOutput = await new Promise((resolve) => {
+    exec('tasklist /FO CSV', (error, stdout) => {
+      if (error) {
+        console.error(error)
+        resolve('')
+        return
+      }
+
+      resolve(stdout)
+    })
+  })
+
+  const parser = parseCsv(tasklistOutput, {
+    columns: true,
+  })
+
+  for await (const record of parser) {
+    if (record['Image Name'].toLowerCase() === processName.toLowerCase()) {
+      return true
+    }
+  }
+
+  return false
 }

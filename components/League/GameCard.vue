a8e7b31751d12da41ac28e0d6fdde5c4e062cd3a
Timo Schwarzer <timo@schwarzer.dev>
2024-03-31 13:35:28 +0200

    Add countdown when game end comes close

--- components/League/GameCard.vue
+++ components/League/GameCard.vue
@@ -1,28 +1,42 @@
 <template>
-  <v-card class="game-card" :class="{'green darken-1': game.isCurrent}" :to="{name: 'league-game-gameId', params: {gameId: game.id}}">
+  <v-card
+    class="game-card"
+    :class="{ accent: game.isCurrent && game.userMetadata?.canSubmit }"
+    :to="{ name: 'league-game-gameId', params: { gameId: game.id } }"
+  >
     <div class="gradient-overlay"></div>
-
-    <div v-if="false" class="card-tag px-2 blue darken-1">
-      Upcoming
-    </div>
 
     <div class="card-content pa-4">
       <div class="game-number">{{ game.gameNumber }}</div>
 
       <div class="spacer"></div>
 
-      <div class="d-flex justify-end">
+      <div class="d-flex flex-column align-end">
         <div>
-          <v-icon small>mdi-flag-checkered</v-icon>
           {{ game.submissionCount }}
+          <v-icon small>mdi-flag-checkered</v-icon>
+        </div>
+        <div v-if="game.userMetadata?.didSubmit">
+          played
+          <v-icon small>mdi-check</v-icon>
         </div>
       </div>
+    </div>
+
+    <div v-if="playableUntil !== null && !game.userMetadata?.didSubmit" class="timer pa-2">
+      <template v-if="countdownTimerText !== null">
+        <span class="font-weight-bold">{{ countdownTimerText }}</span
+        ><br />
+        left to finish this game!
+      </template>
+      <template v-else> Finish this game until {{ formatDateEpoch(playableUntil, 'dd.MM.yyyy HH:mm') }} </template>
     </div>
   </v-card>
 </template>
 
 <script>
   import { formatsDates } from '~/assets/lib/formatsDates'
+  import { formatTime } from '~/assets/lib/formatTime'
 
   export default {
     name: 'LeagueGameCard',
@@ -32,9 +46,42 @@
         type: Object,
         required: true,
       },
+      playableUntil: {
+        type: Number,
+        default: null,
+      },
     },
     data: () => ({
+      countdownTimerText: null,
+      updateIntervalId: null,
     }),
+    mounted() {
+      this.updateIntervalId = setInterval(() => this.updateTimerText(), 1000)
+      this.updateTimerText()
+    },
+    beforeDestroy() {
+      if (this.updateIntervalId !== null) {
+        clearInterval(this.updateIntervalId)
+      }
+    },
+    methods: {
+      updateTimerText() {
+        if (!this.playableUntil) {
+          this.countdownTimerText = null
+          return
+        }
+
+        const secondsLeft = (this.playableUntil - Date.now()) / 1000
+
+        // Only show countdown for <48h
+        if (secondsLeft > 48 * 3600) {
+          this.countdownTimerText = null
+          return
+        }
+
+        this.countdownTimerText = formatTime(secondsLeft, 0, true)
+      },
+    },
   }
 </script>
 
@@ -79,6 +126,13 @@
       top: 0;
       right: 0;
       border-bottom-left-radius: 0.4em;
+    }
+
+    .timer {
+      text-align: center;
+      position: relative;
+      background-color: rgba(0, 0, 0, 0.1);
+      line-height: 1.2;
     }
   }
 </style>
--- pages/league/seasons/_seasonId.vue
+++ pages/league/seasons/_seasonId.vue
@@ -1,5 +1,12 @@
 <template>
   <v-container>
+    <v-slide-y-transition>
+      <div v-if="!!leagueSeason?.backgroundImageUrl" class="background-image-wrapper">
+        <img class="background-image" alt="" :src="leagueSeason.backgroundImageUrl" />
+        <div class="overlay"></div>
+      </div>
+    </v-slide-y-transition>
+
     <throttled-spinner>
       <div v-if="leagueSeason !== null">
         <h1 class="text-center mt-12 mb-6">{{ leagueSeason.name }}</h1>
@@ -46,7 +53,7 @@
           </v-card>
           <div>
             <div class="games-list">
-              <league-game-card v-if="currentGame !== null" :game="currentGame" />
+              <league-game-card v-if="currentGame !== null" :game="currentGame" :playable-until="leagueSeason.nextContinuationAt" />
               <h3 v-if="pastGames.length > 0" class="text-center" :class="{'mt-3': currentGame !== null}">Past Games</h3>
               <v-data-table disable-pagination hide-default-footer /> <!-- TODO -->
             </div>
@@ -144,6 +151,28 @@
 </script>
 
 <style lang="scss" scoped>
+  .background-image-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: -10;
+
+    .background-image {
+      width: 100%;
+      height: 100%;
+      mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.0) 100%);
+    }
+
+    .overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+  }
+
   .tables-container {
     display: grid;
     grid-template-columns: 2fr 1fr;

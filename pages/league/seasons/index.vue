6624ad55310642469e2d0b5ae7582d53aa1f6043
Mawe <MalteWerner.Service@outlook.de>
2024-04-02 01:54:29 +0200

    fix userIsMemberOfSeason

--- pages/league/seasons/index.vue
+++ pages/league/seasons/index.vue
@@ -1,19 +1,15 @@
 <template>
   <v-container>
-    <h1 class='text-center mt-12 mb-6'>Randomizer League</h1>
-    <div class="mb-3">
+    <h1 class="text-center mt-12 mb-12">Randomizer League</h1>
+    <div>
       Small league introduction<br />
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      Cursus mattis molestie a iaculis. Viverra justo nec ultrices dui.
-      Sagittis purus sit amet volutpat consequat mauris nunc.
-      Nulla aliquet enim tortor at auctor. Nibh tellus molestie nunc non.
-      Libero enim sed faucibus turpis. Eget arcu dictum varius duis at
-      consectetur lorem donec massa. Cursus vitae congue mauris rhoncus
-      aenean vel elit. Elementum pulvinar etiam non quam lacus suspendisse
-      faucibus interdum. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris.
-      Mi proin sed libero enim. Etiam non quam lacus suspendisse faucibus interdum
-      posuere lorem ipsum. Auctor elit sed vulputate mi sit.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Cursus mattis molestie a iaculis. Viverra justo nec ultrices dui. Sagittis purus sit amet volutpat
+      consequat mauris nunc. Nulla aliquet enim tortor at auctor. Nibh tellus molestie nunc non. Libero enim sed
+      faucibus turpis. Eget arcu dictum varius duis at consectetur lorem donec massa. Cursus vitae congue mauris rhoncus
+      aenean vel elit. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Vitae congue mauris
+      rhoncus aenean vel elit scelerisque mauris. Mi proin sed libero enim. Etiam non quam lacus suspendisse faucibus
+      interdum posuere lorem ipsum. Auctor elit sed vulputate mi sit.
     </div>
     <v-btn class="mb-3" small text outlined @click="showLeagueInfo = true">
       <v-icon left>mdi-information-outline</v-icon>
@@ -24,8 +20,20 @@
       <h2 class="mt-5 mb-2">Active & Upcoming</h2>
 
       <div class="seasons-container">
-        <league-season-card v-for="season in categorizedSeasons.active" :key="season.id" :season="season" mode="active" :joined="userIsMemberOfSeason(season)" />
-        <league-season-card v-for="season in categorizedSeasons.upcoming" :key="season.id" :season="season" mode="upcoming" :joined="userIsMemberOfSeason(season)"/>
+        <league-season-card
+          v-for="season in categorizedSeasons.active"
+          :key="season.id"
+          :season="season"
+          mode="active"
+          :joined="userIsMemberOfSeason(season)"
+        />
+        <league-season-card
+          v-for="season in categorizedSeasons.upcoming"
+          :key="season.id"
+          :season="season"
+          mode="upcoming"
+          :joined="userIsMemberOfSeason(season)"
+        />
       </div>
     </template>
 
@@ -37,19 +45,15 @@
     </template>
 
     <v-dialog v-model="showLeagueInfo" max-width="800">
-      <v-card class="pa-5">
-        More info here.
-      </v-card>
+      <v-card class="pa-5"> More info here; also rules. WIP. Pls help </v-card>
     </v-dialog>
   </v-container>
 </template>
 
 <script>
-
   // TODO:
   // league description
   // general league ruleset
-  // mark season if: you joined it, its active
   // update list by seasons change (for example new season added, season ended, member joined)
 
   // IDEA:
@@ -100,10 +104,10 @@
 
         this.seasonsLoading = false
       },
-      userIsMemberOfSeason(season){
-        return season.memberships?.some((m) => m.user.id === this.user.id)
+      userIsMemberOfSeason(season) {
+        return season.memberships?.some((m) => m.user.id === this.user?.id)
       },
-    }
+    },
   }
 </script>
 

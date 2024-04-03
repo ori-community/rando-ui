f4daf0017c60092cd17876e87261c017e49fcf77
Timo Schwarzer <timo@schwarzer.dev>
2024-04-03 20:54:39 +0200

    Fix Reset Local Tracker Position button

--- components/WotwRandoSettings.vue
+++ components/WotwRandoSettings.vue
@@ -439,10 +439,10 @@
         }
       },
       async resetLocalTrackerPosition() {
-        this.settings.LocalTracker.X = 0
-        this.settings.LocalTracker.Y = 0
-        this.settings.LocalTracker.Width = 700
-        this.settings.LocalTracker.Height = 405
+        this.settings['LocalTracker.X'] = 0
+        this.settings['LocalTracker.Y'] = 0
+        this.settings['LocalTracker.Width'] = 700
+        this.settings['LocalTracker.Height'] = 405
 
         await window.electronApi.invoke('localTracker.resetWindowRect')
 

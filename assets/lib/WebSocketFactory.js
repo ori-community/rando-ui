f150064c4804dca40bd671b8dca253f41ced39f6
Timo Schwarzer <timo@schwarzer.dev>
2024-05-09 20:45:03 +0200

    Set protocol version to 4.8.0

--- assets/lib/WebSocketFactory.js
+++ assets/lib/WebSocketFactory.js
@@ -15,7 +15,7 @@ export class WebSocketFactory {
           jwt: this.jwt,
           clientVersion: isElectron()
             ? await window.electronApi.invoke('updater.getVersion')
-            : '4.4.2',
+            : '4.8.0',
         }))
 
         resolve(ws)

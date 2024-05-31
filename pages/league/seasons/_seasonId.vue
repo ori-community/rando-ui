702dcef4d2259874550856e01a3c9e276aeac4b1
Mawe <MalteWerner.Service@outlook.de>
2024-05-31 23:03:57 +0200

    fix season start card

--- pages/league/seasons/_seasonId.vue
+++ pages/league/seasons/_seasonId.vue
@@ -593,54 +593,54 @@
     }
   }
 
+  .season-start-container {
+    position: relative;
+    overflow: hidden;
+    border: 1px solid rgba(255, 255, 255, 0.4);
+    background-color: transparent;
+
+    .background-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: var(--v-background-lighten1);
+      opacity: 0.6;
+    }
+
+    .gradient-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 2;
+      pointer-events: none;
+      background: linear-gradient(
+        45deg,
+        rgba(0, 0, 0, 0.6) 0%,
+        rgba(0, 0, 0, 0.05) 40%,
+        rgba(0, 0, 0, 0.05) 60%,
+        rgba(0, 0, 0, 0.6) 100%
+      );
+    }
+
+    .starting-date-content {
+      position: relative;
+      z-index: 1;
+
+      .starting-date {
+        font-size: 2em;
+        font-weight: 600;
+      }
+    }
+  }
+
   .games-list {
     display: flex;
     flex-direction: column;
     gap: 0.75em;
-
-    .season-start-container {
-      position: relative;
-      overflow: hidden;
-      border: 1px solid rgba(255, 255, 255, 0.4);
-      background-color: transparent;
-
-      .background-overlay {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background: var(--v-background-lighten1);
-        opacity: 0.6;
-      }
-
-      .gradient-overlay {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        z-index: 2;
-        pointer-events: none;
-        background: linear-gradient(
-          45deg,
-          rgba(0, 0, 0, 0.6) 0%,
-          rgba(0, 0, 0, 0.05) 40%,
-          rgba(0, 0, 0, 0.05) 60%,
-          rgba(0, 0, 0, 0.6) 100%
-        );
-      }
-
-      .starting-date-content {
-        position: relative;
-        z-index: 1;
-
-        .starting-date {
-          font-size: 2em;
-          font-weight: 600;
-        }
-      }
-    }
   }
 
   .top-row-button {

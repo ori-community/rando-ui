f9aa799554f4e20b28fcd80224167eb77174671d
Timo Schwarzer <timo@schwarzer.dev>
2024-06-02 20:56:01 +0200

    Improve grid spec on home page

--- pages/electron/index.vue
+++ pages/electron/index.vue
@@ -370,7 +370,7 @@
 
   .seasons-container {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
     grid-auto-rows: 1fr;
     gap: 0.75em;
   }

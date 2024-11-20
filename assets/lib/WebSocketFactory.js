fb74e0610fab23cf84bb523f28cc592c481fbf68
Timo Schwarzer <timo@schwarzer.dev>
2024-11-20 20:13:13 +0100

    Update protocol version

--- assets/lib/WebSocketFactory.js
+++ assets/lib/WebSocketFactory.js
@@ -15,7 +15,7 @@ export class WebSocketFactory {
           jwt: this.jwt,
           clientVersion: isElectron()
             ? await window.electronApi.invoke('updater.getVersion')
-            : '4.23.3',
+            : '4.25.0',
         }))
 
         resolve(ws)

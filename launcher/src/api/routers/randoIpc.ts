bcd00e16ef2c2f84fe9b0bceeebfb9a7d11718a9
Timo Schwarzer <timo@schwarzer.dev>
2025-06-18 21:09:23 +0200

    Add RandoIPC API

---
+++ launcher/src/api/routers/randoIpc.ts
@@ -0,0 +1,21 @@
+import {publicProcedure, router} from "../trpc"
+import {RandoIPCService} from "../../services/RandoIPCService"
+import {observable} from "@trpc/server/observable"
+
+export const randoIpc = router({
+  isConnected: publicProcedure
+    .query(async () => {
+      return RandoIPCService.isConnected()
+    }),
+  onConnectionStateChanged: publicProcedure
+    .subscription(() => {
+      return observable<boolean>((emit) => {
+        const onConnectionStateChanged = (connected: boolean) => emit.next(connected)
+        RandoIPCService.events.on("connectionStateChanged", onConnectionStateChanged)
+
+        return () => {
+          RandoIPCService.events.off("connectionStateChanged", onConnectionStateChanged)
+        }
+      })
+    }),
+})

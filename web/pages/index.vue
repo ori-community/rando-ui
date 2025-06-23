c20d0831336d972e7aa4124a3ed00b871325a8d4
Mawe <MalteWerner.Service@outlook.de>
2025-06-23 22:12:19 +0200

    removed tests

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -2,31 +2,11 @@
   <v-container>
     Hi bitte Injector.exe doppelklick und so warnungen ignorieren have fun!
 
-    <v-btn @click="test1">Click</v-btn>
-
-    {{ time }}
   </v-container>
 </template>
 
 <script lang="ts" setup>
-  const electronApi = useElectronApi()
-  const time = ref(0)
 
-  async function test1() {
-    await electronApi.auth.startOAuthFlow.query({
-      apiBaseUrl: 'https://wotw.orirando.com',
-      forceWindowLogin: false,
-    })
-  }
-
-  onMounted(() => {
-    electronApi.timer.onTick.subscribe(undefined, {
-      onData(value: number) {
-        console.log("Timer:", value)
-        time.value = value
-      },
-    })
-  })
 </script>
 
 <style lang="scss" scoped>

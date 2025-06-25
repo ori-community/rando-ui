fb999717abdad2965e0cec4b75459af1151544b0
Mawe <MalteWerner.Service@outlook.de>
2025-06-25 22:25:37 +0200

    added theme variations

--- web/plugins/vuetify.ts
+++ web/plugins/vuetify.ts
@@ -1,31 +1,36 @@
 // import this after install `@mdi/font` package
-import '@mdi/font/css/materialdesignicons.css'
+import "@mdi/font/css/materialdesignicons.css"
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import colors from 'vuetify/util/colors'
+import "vuetify/styles"
+import {createVuetify} from "vuetify"
+import colors from "vuetify/util/colors"
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     theme: {
-      defaultTheme: 'dark',
+      defaultTheme: "dark",
       themes: {
         dark: {
           dark: true,
           colors: {
-            background: '#050E17',
-            primary: '#7ec6f9',
-            secondary: '#155BA2',
-            accent: '#533CA6',
-            accent2: '#D989D3',
-            info: '#5199CD',
+            background: "#050E17",
+            primary: "#7ec6f9",
+            secondary: "#155BA2",
+            accent: "#533CA6",
+            accent2: "#D989D3",
+            info: "#5199CD",
             warning: colors.orange.base,
             error: colors.deepOrange.accent4,
             success: colors.green.accent3,
           },
         },
       },
-    }
+      variations: {
+        colors: ["background", "primary", "accent", "info", "warning", "error", "success"],
+        lighten: 5,
+        darken: 5,
+      },
+    },
   })
   app.vueApp.use(vuetify)
 })

efaf40640c328e6eec277ceb8a3160e78e01a6da
Timo Schwarzer <timo@schwarzer.dev>
2025-07-04 00:00:01 +0200

    Add error page

---
+++ web/error.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+    <v-main class="content">
+      <v-container class="fill-height flex-column align-center justify-center">
+        <img src="@shared/images/ori_shy.png">
+        <h1 class="mb-4">Something went oribly wrong.</h1>
+
+        <v-btn
+          v-if="error.statusCode === 404"
+          :to="homeRoute"
+          prepend-icon="mdi-home"
+          color="accent"
+          variant="flat"
+          class="mb-2"
+        >
+          Go home
+        </v-btn>
+        <div v-else class="mb-3">Press
+          <v-kbd>Ctrl</v-kbd>
+          +
+          <v-kbd>R</v-kbd>
+          to reload.
+        </div>
+
+        <v-expand-transition>
+          <div v-if="showError" class="error-container">
+            <v-code class="error-code">{{ error.stack }}</v-code>
+          </div>
+          <v-btn v-else variant="text" @click="showError = true">Show error</v-btn>
+        </v-expand-transition>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import type {NuxtError} from "#app"
+
+  defineProps<{
+    error: NuxtError
+  }>()
+
+  const showError = ref(false)
+
+  const homeRoute = computed(() => useIsElectron()
+    ? {
+      name: "electron",
+    }
+    : {
+      name: "index",
+    },
+  )
+</script>
+
+<style lang="scss" scoped>
+  .error-container {
+    max-width: 100%;
+
+    .error-code {
+      display: block;
+      white-space: pre;
+      overflow: auto;
+    }
+  }
+</style>
---
+++ web/pages/electron/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-center">
+    here be dragons
+    <br>
+    <img
+      src="https://static.vecteezy.com/system/resources/previews/024/043/921/original/cute-dragon-clipart-transparent-background-free-png.png"
+      alt="dragon" width="200">
+  </div>
+</template>
+<script setup lang="ts">
+</script>

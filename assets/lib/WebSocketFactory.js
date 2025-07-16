d1dd783ba4f2c2d1c03be987478107faa3a976f7
Timo Schwarzer <timo@schwarzer.dev>
2025-07-16 22:17:54 +0200

    Bump protocol version to 4.33.0

--- assets/lib/WebSocketFactory.js
+++ assets/lib/WebSocketFactory.js
@@ -15,7 +15,7 @@ export class WebSocketFactory {
           jwt: this.jwt,
           clientVersion: isElectron()
             ? await window.electronApi.invoke('updater.getVersion')
-            : '4.31.6',
+            : '4.33.0',
         }))
 
         resolve(ws)

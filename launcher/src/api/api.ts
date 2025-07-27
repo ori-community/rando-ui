355122d84542d8cd1616810ca4b34316bfcaf2c8
Timo Schwarzer <timo@schwarzer.dev>
2025-07-27 17:40:38 +0200

    Fix tRPC type inferring

--- launcher/src/api/api.ts
+++ launcher/src/api/api.ts
@@ -1,11 +1,13 @@
-import {router} from "@launcher/api/trpc"
-import {auth} from "@launcher/api/routers/auth"
-import {localTracker} from "@launcher/api/routers/localTracker"
-import {localTrackerWebSocket} from "@launcher/api/routers/localTrackerWebSocket"
-import {randoIpc} from "@launcher/api/routers/randoIpc"
-import {settings} from "@launcher/api/routers/settings"
-import {systemDialogs} from "@launcher/api/routers/systemDialogs"
-import {updater} from "@launcher/api/routers/updater"
+// DO NOT use the @launcher import alias in this file because its type
+// is imported from the front end too!
+import {router} from "./trpc"
+import {auth} from "./routers/auth"
+import {localTracker} from "./routers/localTracker"
+import {localTrackerWebSocket} from "./routers/localTrackerWebSocket"
+import {randoIpc} from "./routers/randoIpc"
+import {settings} from "./routers/settings"
+import {systemDialogs} from "./routers/systemDialogs"
+import {updater} from "./routers/updater"
 
 export const appRouter = router({
   auth,

437450227c50d9996d2f673b9c2e9bebe855dbc7
Mawe <MalteWerner.Service@outlook.de>
2026-01-02 21:19:01 +0100

    temp button to launch seed

--- web/app/pages/electron/index.vue
+++ web/app/pages/electron/index.vue
@@ -2,10 +2,22 @@
   <v-container>
     <div class="text-center">
       <v-btn :loading="isLaunching" @click="launch()">Lauch</v-btn>
+      <v-btn :loading="isLaunching" @click="selectAndLaunchFile()">Saatauswahl</v-btn>
     </div>
   </v-container>
 </template>
 
 <script setup lang="ts">
+
+  const electronApi = useElectronApi()
+
   const {isLaunching, launch} = useLauncherHelper()
+  const selectAndLaunchFile = (async () => {
+    const newPath = await electronApi?.systemDialogs.pickFile.query({
+      filters: [{name: 'Seedfiles', extensions: ['wotwr']}],
+    })
+    if (newPath) {
+      launch(`file:${newPath}`)
+    }
+  })
 </script>

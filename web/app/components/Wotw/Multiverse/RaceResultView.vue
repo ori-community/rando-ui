616d40120efd855bed98326a82fc11fa9cce5e0c
Mawe <MalteWerner.Service@outlook.de>
2026-01-10 22:57:03 +0100

    fix race result view formatting

--- web/app/components/Wotw/Multiverse/RaceResultView.vue
+++ web/app/components/Wotw/Multiverse/RaceResultView.vue
@@ -2,31 +2,36 @@
   <div>
     <h2 class="text-right mb-4">Race result</h2>
 
-    <transition-group class="teams" name="list">
-      <div
-        v-for="(team, index) in sortedRaceTeams"
-        :key="team.id"
-        class="team"
-      >
-        <rando-place-badge :place="team.finishedTime === 0.0 ? 'F' : index + 1"/>
-        <v-card
-          color="background-lighten-1"
-          elevation="0"
-          class="pa-4"
+    <transition-group name="list">
+      <div class="teams">
+        <div
+          v-for="(team, index) in sortedRaceTeams"
+          :key="team.id"
+          class="team"
         >
-          <div class="team-members">
-            <div v-for="member in team.members" :key="member.user.id" class="team-member">
-              <wotw-multiverse-player-view :user="member.user"/>
-              <div class="member-time">{{ member.finishedTime !== 0.0 ? formatTime(member.finishedTime) : 'DNF' }}</div>
+          <rando-place-badge :place="team.finishedTime === 0.0 ? 'F' : index + 1"/>
+          <v-card
+            color="background-lighten-1"
+            elevation="0"
+            class="pa-4"
+          >
+            <div class="team-members">
+              <div v-for="member in team.members" :key="member.user.id" class="team-member">
+                <wotw-multiverse-player-view :user="member.user"/>
+                <div class="member-time">{{
+                    member.finishedTime !== 0.0 ? formatTime(member.finishedTime) : 'DNF'
+                  }}
+                </div>
+              </div>
             </div>
-          </div>
 
-          <div class="team-info mt-5">
-            <div v-if="team.members.length > 1" class="team-time">
-              {{ team.finishedTime !== 0.0 ? formatTime(team.finishedTime) : 'DNF' }}
+            <div class="team-info mt-5">
+              <div v-if="team.members.length > 1" class="team-time">
+                {{ team.finishedTime !== 0.0 ? formatTime(team.finishedTime) : 'DNF' }}
+              </div>
             </div>
-          </div>
-        </v-card>
+          </v-card>
+        </div>
       </div>
     </transition-group>
   </div>
